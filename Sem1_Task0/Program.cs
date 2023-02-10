291654f06dc98ed9393cd9d70d1c8d453c7b1c7b
Reikis-oss <ufnbkby@mail.ru>
2023-02-10 22:17:43 +0300

    add solution for task 0,1,3,5,7

---
+++ Sem1_Task0/Program.cs
@@ -0,0 +1,11 @@
+﻿//  Задача №0
+//  Напишите программу, которая на вход принимает число и
+//  выдаёт его квадрат (число умноженное само на себя)
+
+Console.Write("Введите число: ");
+
+int number = Convert.ToInt32(Console.ReadLine());
+double number_Pow2 = Math.Pow(number, 2);
+
+Console.WriteLine($"Квадрат числа {number} = {number_Pow2}");
+
