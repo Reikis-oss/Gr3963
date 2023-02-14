0f7b1ff40be21801ff439d0a528b8ae5422ca1ed
Reikis-oss <ufnbkby@mail.ru>
2023-02-15 00:51:20 +0300

    add solution for task 10,13,15

---
+++ Sem2_Task10/Program.cs
@@ -0,0 +1,36 @@
+﻿/*
+            Задача 10: Напишите программу, которая принимает на вход 
+            трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+            564  ->   6
+*/
+int num = 0;
+
+for (; ; )
+{
+    Console.Clear();
+    Console.Write("Введите трёхзначное число: ");
+    num = Convert.ToInt32(Console.ReadLine());
+    if ((num > 99) && (num < 1000))
+    {
+        break;
+    }
+    else
+    {
+        Console.WriteLine("Вы ввели не трёхзначное число " +
+                            "\nПопробуйте снова");
+        Console.WriteLine("\n\nНажмите любую кнопку для продолжения");
+        Console.ReadKey();
+    }
+}
+
+string stringNum = Convert.ToString(num);
+Console.WriteLine($"Вторая цифра числа {num} = {stringNum[1]}");
+
+
+
+Console.WriteLine("\n\n\nНажмите любую кнопку для продолжения");
+Console.ReadLine();
+
+
+
---
+++ Sem2_Task13/Program.cs
@@ -0,0 +1,32 @@
+﻿/*
+                Задача 13: Напишите программу, которая выводит 
+                третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+                564         ->  6
+                541546584   ->  1
+*/
+
+for (; ; )
+{
+    Console.Clear();
+    Console.Write("Введите трёхзначное число: ");
+    int num = Convert.ToInt32(Console.ReadLine());
+    int numLength = num.ToString().Length;
+    if (numLength > 2)
+    {
+        int i = 0;
+        for (; i < numLength - 3; i++)
+        {
+        }
+        num = (int)(num / Math.Pow(10, i));
+        Console.WriteLine($"Третья цифра заданного числа = {num % 10}");
+        break;
+    }
+    else
+    {
+        Console.WriteLine("Третьей цифры нет" +
+                                "\nПопробуйте ещё раз");
+        Console.WriteLine("\n\nНажмите любую кнопку для продолжения");
+        Console.ReadKey();
+    }
+}
