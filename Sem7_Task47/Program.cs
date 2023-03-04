1d7dce270645573b254428a02578e32118a4347f
Reikis-oss <ufnbkby@mail.ru>
2023-03-05 02:54:57 +0300

    add the solution for task 47,50,52

---
+++ Sem7_Task47/Program.cs
@@ -0,0 +1,66 @@
+﻿/*
+Задача 47. Задайте двумерный массив размером m×n, 
+заполненный случайными вещественными числами.
+
+*При выводе матрицы показывать каждую цифру разного цвета(цветов всего 16)
+*/
+
+Console.Clear();
+
+//Принимаем из консоли число
+int ReadInputInt(string mess)
+{
+    Console.Write(mess);
+    return Convert.ToInt32(Console.ReadLine() ?? "0");
+}
+
+//Генерируем массив случайными числами
+double[,] Gen2DArrRandom(int row, int colum, int bottom, int top)
+{
+    Random rnd = new Random();
+    double[,] arr = new double[row, colum];
+
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < colum; j++)
+        {
+            arr[i, j] = Math.Round(rnd.Next(bottom, top) + rnd.NextDouble(), 2);
+        }
+    }
+    return arr;
+}
+
+// выводим двумерный массив на экран с разукрашиванием элемента если он равен тому что ввёл пользователь
+void Print2DArr(double[,] arr)
+{
+    int colorIndex = 0;
+
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            char[] digitArray = arr[i, j].ToString().ToCharArray();
+
+                for (int t = 0; t < digitArray.Length; t++)
+                {
+                    Console.ForegroundColor = (ConsoleColor)colorIndex;
+                    Console.Write(digitArray[t]);
+                    colorIndex = (colorIndex + 1) % 16;
+                } 
+            Console.Write("\t");
+            
+        }
+        Console.WriteLine();
+    }
+    Console.ResetColor();
+}
+
+
+int countRow = ReadInputInt("Введите количество строк для двумерного массива: ");
+int countColum = ReadInputInt("Введите количество столбцов для двумерного массива: ");
+int min = ReadInputInt("Введите 1 границу последовательности: ");
+int max = ReadInputInt("Введите 2 границу последовательности: ");
+if (min > max) (min, max) = (max, min);
+
+double[,] mas = Gen2DArrRandom(countRow, countColum, min, max);
+Print2DArr(mas);
---
+++ Sem7_Task50/Program.cs
@@ -0,0 +1,183 @@
+﻿Console.Clear();
+
+//Принимаем из консоли число
+int ReadInputInt(string mess)
+{
+    Console.Write(mess);
+    return Convert.ToInt32(Console.ReadLine() ?? "0");
+}
+
+//Создаём двумерный массив из чисел Фмбоначчи
+int[,] Gen2DArrFibonachi(int row, int colum)
+{
+    int[,] arr = new int[row, colum]; // создаем двумерный массив для хранения чисел Фибоначчи
+                                      // первые два числа Фибоначчи равны 1
+    arr[0, 0] = 0;
+    arr[0, 1] = 1;
+
+
+    if (colum > 2)
+    {
+        // заполняем первую строку массива остальными числами Фибоначчи
+        for (int i = 2; i < colum; i++)
+        {
+            arr[0, i] = arr[0, i - 1] + arr[0, i - 2];
+        }
+    }
+
+    // заполняем остальные строки массива на основе предыдущей строки
+    for (int i = 1; i < row; i++)
+    {
+        for (int j = 0; j < colum; j++)
+        {
+            if (j == 0)
+            {
+                arr[i, j] = arr[i - 1, colum - 2] + arr[i - 1, colum - 1];
+            }
+            else if (j == 1)
+            {
+                arr[i, j] = arr[i, j - 1] + arr[i - 1, colum - 1];
+            }
+            else
+            {
+                arr[i, j] = arr[i, j - 1] + arr[i, j - 2];
+            }
+        }
+    }
+
+
+
+    return arr;
+}
+
+// выводим двумерный массив на экран с разукрашиванием элемента если он равен тому что ввёл пользователь
+void Print2DArr(int[,] arr, int num)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            if (arr[i, j] == num)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.Write(arr[i, j] + "\t");
+                Console.ResetColor();
+            }
+            else
+            {
+                Console.Write(arr[i, j] + "\t");
+            }
+        }
+        Console.WriteLine();
+    }
+
+}
+
+//Создаём одномерный массив из чисел Фмбоначчи
+int[] Gen1DArrFibonachi(int len)
+{
+    int[] arr = new int[len];
+    arr[0] = 0;
+    arr[1] = 1;
+
+    for (int i = 2; i < len; i++)
+    {
+        arr[i] = arr[i - 1] + arr[i - 2];
+    }
+
+    return arr;
+}
+
+//Выводим массив на экран в 1 строку
+void Print1DArrLine(int[] arr, int num)
+{
+    Console.Write("[");
+
+    for (int i = 0; i < arr.Length - 1; i++)
+    {
+        if (num == arr[i])
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.Write($"{arr[i]},\t");
+            Console.ResetColor();
+        }
+        else
+        {
+            Console.Write($"{arr[i]},\t");
+        }
+    }
+    if (num == arr[arr.Length - 1])
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.Write($"{arr[arr.Length - 1]}");
+        Console.ResetColor();
+        Console.WriteLine("]");
+    }
+    else
+    {
+        Console.WriteLine($"{arr[arr.Length - 1]}]");
+    }
+}
+
+//Выводим массив на экран в 1 линию
+void Print1DArrСolumn(int[] arr, int num)
+{
+
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (num == arr[i])
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.Write($"{arr[i]}\n");
+            Console.ResetColor();
+        }
+        else
+        {
+            Console.Write($"{arr[i]}\n");
+        }
+    }
+}
+
+int countRow = ReadInputInt("Введите количество строк для двумерного массива: ");
+int countColum = ReadInputInt("Введите количество столбцов для двумерного массива: ");
+int numer = ReadInputInt("Введите искомое число: ");
+
+if (countRow == 1 && countColum == 1)
+{
+    if (numer == 0)
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine(0);
+        Console.ResetColor();
+    }
+    else
+    {
+        Console.WriteLine(0);
+    }
+
+}
+else if (countRow == 1 || countColum == 1)
+{
+
+
+    if (countRow == 1)
+    {
+        int[] fbn = Gen1DArrFibonachi(countColum);
+        Print1DArrLine(fbn, numer);
+    }
+
+    if (countColum == 1)
+    {
+        int[] fbn = Gen1DArrFibonachi(countRow);
+        Print1DArrСolumn(fbn, numer);
+    }
+}
+else if (countRow > 1 && countColum > 1)
+{
+    int[,] fbn = Gen2DArrFibonachi(countRow, countColum);
+    Print2DArr(fbn, numer);
+}
+
+
+
+
