cbf5055213734333ceac657e0ee5cb45b97aa357
Reikis-oss <ufnbkby@mail.ru>
2023-03-11 19:53:13 +0300

    add the solution for task 41, 43

---
+++ Sem6_Task41/Program.cs
@@ -0,0 +1,51 @@
+﻿/*
+  
+   Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+    0, 7, 8, -2, -2 -> 2
+
+    1, -7, 567, 89, 223-> 3
+
+    Пользователь вводит число нажатий, затем программа следит за нажатиями и выдает сколько чисел больше 0 было введено.
+ */
+
+int count = ReadCount(); // считываем количество нажатий
+
+int positiveCount = CountPositiveNumbers(count); // считаем количество положительных чисел
+Console.WriteLine($"Количество положительных чисел: {positiveCount}");
+
+
+int ReadCount()
+{
+    Console.Write("Введите количество нажатий: ");
+    int count;
+    while (!int.TryParse(Console.ReadLine(), out count) || count < 1)
+    {
+        Console.Write("Некорректный ввод, попробуйте еще раз: ");
+    }
+    return count;
+}
+
+
+int CountPositiveNumbers(int count)
+{
+    int positiveCount = 0;
+    int prevNumber = 0;
+    for (int i = 0; i < count; i++)
+    {
+        ConsoleKeyInfo key = Console.ReadKey(true);
+        Console.Write(Console.ReadKey().KeyChar);
+        
+        if (key.KeyChar >= '0' && key.KeyChar <= '9')
+        {
+            int number = int.Parse(key.KeyChar.ToString());
+            if (prevNumber < 0)
+            {
+                number *= -1;
+            }
+            positiveCount += (number > 0) ? 1 : 0;
+            prevNumber = number;
+        }
+    }
+    return positiveCount;
+}
---
+++ Sem6_Task43/Program.cs
@@ -0,0 +1,39 @@
+﻿/*
+Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, 
+Заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; 
+Значения b1, k1, b2 и k2 задаются пользователем.
+
+b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+*Найдите площадь треугольника образованного пересечением 3 прямых
+*/
+
+//Принимаем из консоли число
+double ReadInputDouble(string mess)
+{
+    Console.Write(mess);
+    return Convert.ToDouble(Console.ReadLine() ?? "0");
+}
+
+void PrindData(string mess)
+{
+    Console.WriteLine(mess);
+}
+
+(double cordX, double cordY) FindIntersectionPoint(double x1Intercepts, double slope1, double x2Intercepts, double slope2)
+{
+    double cordX = (x2Intercepts - x1Intercepts) / (slope1 - slope2);
+    double cordY = slope1 * cordX + x1Intercepts;
+    return (cordX, cordY);
+}
+
+
+double b1 = ReadInputDouble("Введите значение для точки пересечения x1 с осью `y`: ");
+double k1 = ReadInputDouble("Введите значение для коэффициента наклона прямой для x1: ");
+double b2 = ReadInputDouble("Введите значение для точки пересечения x2 с осью `y`: ");
+double k2 = ReadInputDouble("Введите значение для коэффициента наклона прямой для x2: ");
+
+(double x, double y) res = FindIntersectionPoint(b1,k1,b2,k2);
+
+PrindData($"Прямые пересекаются в точке: ({res.x};{res.y})");
+
