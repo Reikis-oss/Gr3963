4c3b36b71b32f3d4354be9bb34c27fcdf4fc339c
Reikis-oss <ufnbkby@mail.ru>
2023-03-13 20:39:26 +0300

    Changing the solution for task 60

--- Sem8_Task60/Program.cs
+++ Sem8_Task60/Program.cs
@@ -8,6 +8,8 @@
 26(1,0,1) 55(1,1,1)
 */
 
+Console.Clear();
+
 //Принимаем из консоли число
 int ReadInputInt(string mess)
 {
@@ -46,13 +48,15 @@ int[,,] Gen3DArrRandomUnique(int row, int colum, int list, int bottom, int top)
 // выводим двумерный массив на экран 
 void Print3DArr(int[,,] arr)
 {
-    for (int i = 0; i < 2; i++)
+    for (int i = 0; i < arr.GetLength(0); i++)
     {
-        for (int j = 0; j < 2; j++)
+        for (int j = 0; j < arr.GetLength(1); j++)
         {
-            for (int k = 0; k < 2; k++)
+            for (int k = 0; k < arr.GetLength(2); k++)
             {
                 Console.Write($"{arr[i, j, k]} ({i}, {j}, {k})\t");
+                Console.Write(arr[i,j,k] +" (" + i + ", " + j +", " + k +")\t");
+                Console.Write("{0} ({1}, {2}, {3})\t", arr[i,j,k], i, j, k);
             }
             Console.WriteLine();
         }
@@ -83,6 +87,7 @@ return false;
 int countRow = ReadInputInt("Введите количество строк для трехмерного массива: ");
 int countColum = ReadInputInt("Введите количество столбцов для трехмерного массива: ");
 int countList = ReadInputInt("Введите количество листов для трехмерного массива: ");
+
 int min = 10;
 int max = 100;
 
